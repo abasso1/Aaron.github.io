48de84c4260aee4ab58c5ff4b09e33f80f4b2a91
Aaron Basso <abasso1@lsu.edu>
2018-02-18 17:38:26 -0600

    changes

---
+++ js/script.js
@@ -0,0 +1,9 @@
+let myNumber = 12
+let myString = 'That good GIS'
+let myBoolean = false
+let myArray = ['square', 'dare', 'mare']
+
+console.log(' My Special Number: ' + myNumber)
+console.log('Text String:' + myString)
+console.log('Boolean Statement:' + myBoolean)
+console.log('Array of words for assingment:' + myArray)
