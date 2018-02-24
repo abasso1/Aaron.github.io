183cba6a22132082fce1e04a35211644d3e3457b
Aaron Basso <abasso1@lsu.edu>
2018-02-23 18:03:03 -0600

    code assignment 04

---
+++ code-assignment-04.js
@@ -0,0 +1,24 @@
+let projectArray = [
+  1,
+  'https://abasso1.github.io',
+  'The Best Website',
+  'This is a website to showcase AB Productions development skills.',
+  true
+  ['AB Productions','The Best Website','That Good']
+]
+let projectObject = {
+  id: 1,
+  url: 'https://abasso1.github.io',
+  title: 'The Best Website',
+  desc: 'This is a website to showcase AB Productions development skills.',
+  thumb: true,
+  keywords: [
+    'AB Productions',
+    'The Best Website',
+    'That Good'
+  ]
+}
+
+console.log(projectArray[1])
+console.log(projectObject.title)
+console.log(projectObject["The Best Website"])
