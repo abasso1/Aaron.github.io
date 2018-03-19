a92a21c75617c53aaeb00f365c743541f6ee4937
Aaron Basso <abasso1@lsu.edu>
2018-03-18 21:03:45 -0500

    new assignment niggas

---
+++ js/school.js
@@ -0,0 +1,7 @@
+function describeSchool (school) {
+  let description = 'At ' + school + ' there is a live tiger on campus.'
+  return description
+}
+let mySchool = 'LSU'
+let description = describeSchool(mySchool)
+console.log(description)
