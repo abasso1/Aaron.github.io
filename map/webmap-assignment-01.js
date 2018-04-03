1236eea6ccaf2c6f8883486f432295d19bfac5b4
Aaron Basso <abasso1@lsu.edu>
2018-04-03 09:46:39 -0500

    webmap 1 comletion

--- map/webmap-assignment-01.js
+++ map/webmap-assignment-01.js
@@ -2,20 +2,27 @@ let aaronmap = L.map('aaronmap').setView([20, -10], 8)
 L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/NatGeo_World_Map/MapServer/tile/{z}/{y}/{x}.png').addTo(aaronmap);
 let aaronmarker = L.marker([69, -69]).addTo(aaronmap);
 let aaronpolygon = L.polygon([
-  [12, 22],
-  [37, 45],
-  [40, 70]
+  [48, -127],
+  [48, -67],
+  [25, -79],
+  [31,-123]
 ]).addTo(aaronmap);
-aaronpolygon.bindPopup('NOT Disney World')
+aaronpolygon.bindPopup('USA! USA! USA! USA!')
 aaronmarker.bindPopup('The dirty white North')
-function logRide (event) {
-  console.log('Best Ride in park' + event.latlng)
-};
-aaronmap.on('CLICK.', logRide);
-var basePolyline = [
-    [20, -14],
-    [80, 10],
-    [0, 1]
+var aaronpolyline=[
+    [.2, -220],
+    [.2, -57],
+    [.2, 79]
 ];
-var aaronpolyline = L.polyline(basePolyline, {color: 'orange'}).addTo(aaronmap);
+var aaronpolyline = L.polyline(aaronpolyline, {color: 'orange'}).addTo(aaronmap);
 aaronmap.fitBounds(aaronpolyline.getBounds());
+aaronmap.on('click', function (event) {
+  console.log(' ' + event.latlng)
+})
+aaronmap.on('click', function(e) {
+        var popLocation= e.latlng;
+        var popup = L.popup()
+        .setLatLng(popLocation)
+        .setContent('<p>There is nothing worth seeing here!<br />Try some other region.</p>')
+        .openOn(aaronmap);
+    });
